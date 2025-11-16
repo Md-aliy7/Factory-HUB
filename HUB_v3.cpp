4267c90ec0473808facc8fb56f22b31a71f0dbce
Md-aliy7 <119819019+Md-aliy7@users.noreply.github.com>
2025-11-16 21:04:51 +0300

    Update HUB_v3.cpp

--- HUB_v3.cpp
+++ HUB_v3.cpp
@@ -1,5 +1,5 @@
 /*
- * Multiprotocol Gateway Hub (V3 - ZMQ Proxy Architecture)
+ * Multiprotocol Gateway Hub (V3 - fully cross-platform for Windows, Linux, and macOS)
  *
  * Architecture (Decoupled PUB/SUB):
  * 1. Main Thread: Runs Dear ImGui loop.
@@ -2619,3 +2619,4 @@ int main(int, char**) {
 
     return 0;
 }
+
